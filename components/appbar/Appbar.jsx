f297a0f2cc4a90e03bfaf53815c251ecba762077
pehjosamali <peh.joseph@amalitech.com>
2023-01-10 22:55:27 +0000

    devfix

--- components/appbar/Appbar.jsx
+++ components/appbar/Appbar.jsx
@@ -43,7 +43,7 @@ return (
 </div>
 <div className="ic">
 <svg onClick={onOpen} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-nested" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z"/>
+<path fillRule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z"/>
 </svg> 
 <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
 <DrawerOverlay />
