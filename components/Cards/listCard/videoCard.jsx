2582ce569a6d0429b2edb6cc0d1fc9a7d0ebf240
pehjosamali <peh.joseph@amalitech.com>
2023-01-12 23:53:50 +0000

    fix

--- components/Cards/listCard/videoCard.jsx
+++ components/Cards/listCard/videoCard.jsx
@@ -13,7 +13,7 @@ function videoCard() {
         
         </div>
         <div className='video_content'>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
         
         </div>
     </div>
